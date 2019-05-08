551459cf6151bd48a84abc468f63b75b28f39816
Joshua Suskin <jsuskin85@gmail.com>
2019-05-08 05:01:36 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,9 +59,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# 
-
 	programmer_hash = 
  		{
  		    :yukihiro_matsumoto => {
